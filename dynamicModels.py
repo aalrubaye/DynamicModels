c9b8b673bf295f2f69467d936dd60a496503dac8
Abdul Rubaye <aalrubaye2013@my.fit.edu>
2018-10-06 01:23:37 -0400

    implemented the death process

--- dynamicModels.py
+++ dynamicModels.py
@@ -8,34 +8,23 @@ class RunModel:
     # create a network instance
     graph = nx.Graph()
     nodes_count = 0
-
     ii = 0
+    pq = [0.8, 0.2]
 
     # initializing the graph with a node that has an edge to itself
     def __init__(self):
         self.graph.add_node(0)
         self.graph.add_edge(0, 0)
+
         self.nodes_count = self.graph.number_of_nodes()
+
+        # let's assume starting with a birth process if there is only one node in the graph
         self.birth_process()
 
     # The implementation of birth process
     def birth_process(self):
-        add_prob_list = []
-
-        # calculating the probability of all nodes
-        for i in range(0, self.graph.number_of_nodes()):
-            node_degree = self.graph.degree[i]
-            node = self.graph.nodes[i]
-
-            # the implementation of equation 1
-            prob = float(node_degree) / float((self.graph.number_of_edges()*2))
-            # creating a list of all nodes' addition probabilities
-            add_prob_list.append(prob)
-
-            print node_degree
-            print add_prob_list[i]
-
-        print ('*'*10)
+        # creating a list of nodes probability for process_type = 1 (birth)
+        add_prob_list = self.nodes_probs(1)
 
         # creating a new node
         new_node_index = self.nodes_count
@@ -50,19 +39,50 @@ class RunModel:
         print self.graph.nodes
         print self.graph.edges
 
-        self.ii +=1
+        self.ii += 1
 
-        if self.ii < 10 :
+        # rr = self.weighted_random_node(self.pq)
+
+        if self.ii < 10:
             self.birth_process()
+
+    # The implementation of death process
+    def death_process(self):
+        # creating a list of nodes probability for process_type = 2 (death)
+        deletion_prob_list = self.nodes_probs(2)
+        print deletion_prob_list
+
+        # finding the preferential node that will be removed from the graph
+        preferential_node = self.weighted_random_node(deletion_prob_list)
+
+        print preferential_node
+        self.graph.remove_node(preferential_node)
+        self.nodes_count -= 1
+        print self.graph.nodes
+        print self.graph.edges
+
+    # calculating the probability of all nodes (process_type = 1 is birth process & process_type = 2 death process)
+    def equations(self, node_degree, process_type):
+        if process_type == 1:
+            return float(node_degree) / float((self.graph.number_of_edges()*2))
+        else:
+            return float(self.graph.number_of_nodes() - node_degree) / float(self.graph.number_of_nodes()**2 - self.graph.number_of_edges()*2)
+
+    # creating a list of nodes' probabilities
+    def nodes_probs(self, process_type):
+        prob_list = []
+
+        for i in range(0, self.graph.number_of_nodes()):
+            node_degree = self.graph.degree[i]
+            prob = self.equations(node_degree, process_type)
+            prob_list.append(prob)
+
+        return prob_list
 
     # This function picks a random index from the list of the nodes based on their preferential probability
     def weighted_random_node(self, add_prob_list):
         rand = np.random.choice(len(add_prob_list), 1, p=add_prob_list)[0]
         return rand
-
-    # The implementation of death process
-    def death_process(self):
-        death = True
 
 
 # The main function
