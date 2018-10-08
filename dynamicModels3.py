90cf6db804fd4903b13c1ba74bc8ce5b04bed38f
Abdul Rubaye <aalrubaye2013@my.fit.edu>
2018-10-08 14:31:59 -0400

    degree distribution added

--- dynamicModels3.py
+++ dynamicModels3.py
@@ -2,6 +2,7 @@ __author__ = 'Abdul Rubaye'
 from igraph import *
 import numpy as np
 import matplotlib.pyplot as plt
+import powerlaw as pl
 
 # Notes:
 # graph: the instance of the generated graph
@@ -18,15 +19,19 @@ graph = Graph()
 nodes = []
 nodes_index = 0
 steps_counter = 0
-limit = 1000
+limit = 5000
 pq = []
 p = [0.6, 0.75, 0.9]
 t_steps = []
 n_counts = []
+m_counts = []
+fig_m = []
+degrees_p9 = []
+
 
 # initializes a graph with a given probability p
 def initialize_graph(prob):
-    global pq, fig_2_steps, fig_2_p_60, fig_2_p_75, fig_2_p_90
+    global pq, fig_m, degrees_p9
 
     pq = [prob, 1-prob]
 
@@ -45,28 +50,26 @@ def initialize_graph(prob):
     print t_steps
     print n_counts
 
-    # fig_2_steps = t_steps
-    # if prob == p[0]:
-    #     fig_2_p_60 = n_counts
-    # if prob ==p[1]:
-    #     fig_2_p_75 = n_counts
-    # else:
-    #     fig_2_p_90 = n_counts
+    fig_t = t_steps
+    fig_n = n_counts
+    fig_m = m_counts
 
-    stp = t_steps
-    fig = n_counts
+    if prob == 0.9:
+        degrees_p9 = graph.degree()
+
     reset_graph()
-    return stp, fig
+    return fig_t, fig_n, fig_m
 
 
 # reset the graph and other related parameters
 def reset_graph():
-    global nodes_index, steps_counter, nodes, n_counts, t_steps
+    global nodes_index, steps_counter, nodes, n_counts, t_steps, m_counts
     graph.delete_vertices(graph.vs)
     nodes_index = 0
     steps_counter = 0
     nodes = []
     n_counts = []
+    m_counts = []
     t_steps = []
 
 
@@ -110,6 +113,7 @@ def add_node(index):
     nodes_index += 1
     t_steps.append(steps_counter)
     n_counts.append(graph.vcount())
+    m_counts.append(graph.ecount())
 
     print 'step =', steps_counter,' --> Birth '
 
@@ -123,6 +127,7 @@ def remove_node(index):
     steps_counter += 1
     t_steps.append(steps_counter)
     n_counts.append(graph.vcount())
+    m_counts.append(graph.ecount())
 
     print 'step =', steps_counter,' --> Death '
 
@@ -178,6 +183,14 @@ def expected_nodes(prob, steps):
     return en
 
 
+# calculated the expected edges count
+def expected_edges(prob, steps):
+    em = []
+    for i in range(0, steps):
+        em.append((prob*(prob-(1-prob)))*i)
+    return em
+
+
 # Prepares the data to be plotted as scatter
 def create_scatter(list, rng):
     s = 0
@@ -199,11 +212,13 @@ def create_scatter(list, rng):
 
 
 # calls all other functions to calculate and prepare data for plotting
-def process(prob):
-    steps, number_of_nods = initialize_graph(prob)
+def create_fig_n_count(prob):
+    steps, number_of_nods, number_of_edges = initialize_graph(prob)
     print ('*'*40)
     en = expected_nodes(prob, len(steps))
+    em = expected_edges(prob, len(steps))
     scatter_steps, scatter_number_of_nodes = create_scatter(number_of_nods, space_between_ticks)
+    scatter_steps, scatter_number_of_edges = create_scatter(number_of_edges, space_between_ticks)
     if prob == p[0]:
         mrkr = 'D'
         lbl = 'P='+str(prob)
@@ -214,8 +229,25 @@ def process(prob):
         mrkr = '^'
         lbl = 'P='+str(prob)
 
-    plt.plot(scatter_steps, scatter_number_of_nodes, mrkr, markersize=10, mfc='none', label =lbl)
+    plt.plot(scatter_steps, scatter_number_of_nodes, mrkr, markersize=10, mfc='none', label=lbl)
     plt.plot(steps, en, '-')
+
+    return [scatter_steps, scatter_number_of_edges, steps, em]
+
+
+def create_fig_m_count(prob, params):
+    if prob == p[0]:
+        mrkr = 'D'
+        lbl = 'P='+str(prob)
+    if prob == p[1]:
+        mrkr = 's'
+        lbl = 'P='+str(prob)
+    if prob == p[2]:
+        mrkr = '^'
+        lbl = 'P='+str(prob)
+
+    plt.plot(params[0], params[1], mrkr, markersize=10, mfc='none', label=lbl)
+    plt.plot(params[2], params[3], '-')
 
 
 # The main function
@@ -224,18 +256,75 @@ if __name__ == "__main__":
     plt.figure(figsize=(16,10))
 
     plt.xticks(np.arange(0, limit, step=space_between_ticks))
+    plt.subplots_adjust(wspace=0.4, hspace=0.3)
 
-
+    # plotting Figure 2 in the paper
     plt.subplot(2, 2, 1)
     plt.xlabel('t', fontsize=18)
-    plt.ylabel('E[n_t]', fontsize=16)
+    plt.ylabel('E[n_t]', fontsize=16, rotation=0, labelpad=30)
     plt.title('Growth in # of nodes')
 
-    process(p[0])
-    process(p[1])
-    process(p[2])
+    fig_m_params_1 = create_fig_n_count(p[0])
+    fig_m_params_2 = create_fig_n_count(p[1])
+    fig_m_params_3 = create_fig_n_count(p[2])
+
+    # print ('degrees')
+    # print degrees_p9
+    srtd = np.sort(degrees_p9)[::-1]
+
+    # for i in range (0,len(degrees_p9)):
+
+    # print srtd
+    srtd_u = np.unique(srtd)
+    s_srted_u = np.sort(srtd_u)[::-1]
+    index_array = []
+    count_array = []
+
+    index_array.append(s_srted_u[0])
+    for i in range (s_srted_u[0]-1,-1,-1):
+        index_array.append(i)
+
+    for i in range(0, len(index_array)):
+        cn = np.count_nonzero(srtd == index_array[i])
+        count_array.append(cn)
+
+    print index_array
+    print count_array
+    counts = 0
+    final_data_to_plot = []
+    for i in range (0, len(index_array)):
+        counts += count_array[i]
+        for k in range(0, counts):
+            final_data_to_plot.append(index_array[i])
+
+    print final_data_to_plot
+
+
 
     plt.legend(loc='upper left', numpoints=1)
+
+    # plotting figure 3 in the paper
+    plt.subplot(2, 2, 2)
+    plt.xlabel('t', fontsize=18)
+    plt.ylabel('E[m_t]', fontsize=16, rotation=0, labelpad=30)
+    plt.title('Growth in # of edges')
+
+    create_fig_m_count(p[0], fig_m_params_1)
+    create_fig_m_count(p[1], fig_m_params_2)
+    create_fig_m_count(p[2], fig_m_params_3)
+
+    plt.legend(loc='upper left', numpoints=1)
+
+    fit = pl.Fit(degrees_p9)
+    label = r'$\alpha = %.2f\pm%.3f$'%(fit.power_law.alpha,2*fit.power_law.sigma)
+    plt.subplot(2, 2, 3)
+    plt.xlabel('k', fontsize=18)
+    plt.ylabel('P[k]', fontsize=16, rotation=0, labelpad=30)
+    plt.title('Cumulative Degree distribution (P = 0.9)')
+    fit = pl.Fit(final_data_to_plot)
+    fit.plot_pdf(marker='s',linestyle='none', color='black', mfc='none', markersize=12)
+    fit.power_law.plot_pdf(label=label,lw=1.4, color='black')
+    plt.legend(loc='upper right', fontsize= 16)
+    plt.tick_params(axis='both', which='minor', labelsize=16)
+
     plt.show()
-
-
